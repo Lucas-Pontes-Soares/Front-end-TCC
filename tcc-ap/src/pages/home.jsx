48d0eb5da034d2b2eb97f926e4f07a533875dd05
Lucas <lucas.soares177@etec.sp.gov.br>
2023-07-24 10:56:55 -0300

    Adicionando responsividade na tela home

--- tcc-ap/src/pages/home.jsx
+++ tcc-ap/src/pages/home.jsx
@@ -36,13 +36,13 @@ export function Home(){
                     <input type="radio" className="inputSlide" name="slider" onChange={()=>{}} id="item-3" />
                     <div className="slides">
                         <label className="slide" htmlFor="item-1" id="song-1" >
-                        <img src= {require('../image/Group 1.png')} alt="slide" />
+                        <img className="imagemSlide" src= {require('../image/Group 1.png')} alt="slide" />
                         </label>
                         <label className="slide" htmlFor="item-2" id="song-2" >
-                        <img src= {require('../image/Group 4 (2).png')} alt="slide" />
+                        <img className="imagemSlide" src= {require('../image/Group 4 (2).png')} alt="slide" />
                         </label>
                         <label className="slide" htmlFor="item-3" id="song-3" >
-                        <img src= {require('../image/Group 3.png')} alt="slide" />
+                        <img className="imagemSlide" src= {require('../image/Group 3.png')} alt="slide" />
                         </label>
                     </div>
                 </div>
@@ -50,13 +50,13 @@ export function Home(){
 
             <div className="plataformas">
                 <div id="divSteam">
-                    <img src= {require('../image/iconSteam.png')} alt="steam" heigth="120px" width="140px"/>
+                    <img className="imagemLogoPlataforma" src= {require('../image/iconSteam.png')} alt="steam" heigth="120px" width="140px"/>
                 </div> 
                 <div id="divXbox">
-                    <img src= {require('../image/iconXbox.png')} alt="xbox" heigth="100px" width="120px"/>
+                    <img className="imagemLogoPlataforma" src= {require('../image/iconXbox.png')} alt="xbox" heigth="100px" width="120px"/>
                 </div>
                 <div id="divPS">
-                    <img src= {require('../image/iconPS.png')} alt="playstation" heigth="140px" width="160px"/>
+                    <img className="imagemLogoPlataforma" src= {require('../image/iconPS.png')} alt="playstation" heigth="140px" width="160px"/>
                 </div>
             </div>
         </div>
